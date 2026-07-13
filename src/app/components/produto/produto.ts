1f00ff160795ada6185a5b5df9042235fe5994aa
adam castro <riuksilva72@gmail.com>
2026-07-13 17:22:48 -0300

    refactor(produto): adiciona imports de UppercasePipe e PrecoFormatadoPipe

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import {UpperCasePipe, CurrencyPipe} from '@angular/common';
+import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe, PrecoFormatadoPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
-produto = 'Notebook';
-preco = 2500;
-mostrarPreco = true;
-mostrarProduto = true;
+//produto = 'Notebook';
+//preco = 2500;
+//mostrarPreco = true;
+//mostrarProduto = true;
+produtos = [
+  {produto: 'Monitor', preco: 1000},
+  {produto: 'Teclado', preco: 49.99},
+  {produto: 'Mouse', preco: 25.59},
+];
 }
