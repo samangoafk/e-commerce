bbb052a9bdd1ba672e7cc5dc0e24bdb9b7945413
adam castro <riuksilva72@gmail.com>
2026-07-14 17:12:23 -0300

    refactor(app): remove importação de Produto e adiciona ListaProdutos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; //remove importação do RoutOutlet, pois não está sendo utilizado no momento;
 import {Produto} from './components/produto/produto'; //adiciona produto
+import { ListaProdutos } from './components/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [ Produto ],
+  imports: [ ListaProdutos ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
