2c69e903b8db16cdd15c2f6130b8c4ccf9658e60
adam castro <riuksilva72@gmail.com>
2026-07-14 17:20:28 -0300

    refactor(lista-produtos): atualiza classe e adiciona coleção ListaProdutos

--- src/app/components/lista-produtos/lista-produtos.ts
+++ src/app/components/lista-produtos/lista-produtos.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { Produto } from '../produto/produto';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [ Produto ],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+export class ListaProdutos {
+produtos =[
+  {nome: 'Teclado Gamer',
+     preco:149.99},
+  {nome: 'Mouse Gamer',
+     preco:299.99},
+  {nome: 'Monitor Gamer',
+     preco:1599.99},
+  {nome: 'Desktop Gamer',
+     preco:4999.99},
+  {nome: 'Headset Gamer',
+     preco:699.99},
+];
+}
