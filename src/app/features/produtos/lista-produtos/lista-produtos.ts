7ed8dbdae87367c51a0ee469716397c8d8875a72
adam castro <riuksilva72@gmail.com>
2026-07-15 16:21:30 -0300

    feat(features) cria uma psta organizadora para comportar os arquivos anteriormente pertencentes a pasta components

---
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Produto } from '../produto/produto';
+
+@Component({
+  selector: 'app-lista-produtos',
+  imports: [ Produto ],
+  templateUrl: './lista-produtos.html',
+  styleUrl: './lista-produtos.css',
+})
+export class ListaProdutos {
+produtos =[
+  {nome: 'Teclado Gamer',
+     preco:149.99},
+  {nome: 'Mouse Gamer',
+     preco:299.99},
+  {nome: 'Monitor Gamer',
+     preco:1599.99},
+  {nome: 'Desktop Gamer',
+     preco:4999.99},
+  {nome: 'Headset Gamer',
+     preco:699.99},
+];
+exibirProduto (nome:string){
+   console.log ('Produto selecionado: ', nome);
+}
+}
