5f621e55bf46a9ef362517d7a90b2c07ba73058c
adam castro <riuksilva72@gmail.com>
2026-07-15 17:05:56 -0300

    feat

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; //remove importação do RoutOutlet, pois não está sendo utilizado no momento;
-import {Produto} from './components/produto/produto'; //adiciona produto
-import { ListaProdutos } from './components/lista-produtos/lista-produtos';
+import {Produto} from './features/produtos/produto/produto'; //adiciona produto
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
   imports: [ ListaProdutos ],
