34611803886caf6acc696cc594804551bc93a8d1
adam castro <riuksilva72@gmail.com>
2026-07-20 16:53:16 -0300

    feat(carrinho): cria uma nova pasta para adicionar aba carrinho e uma nova página

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //Logica do carrinho virá em aulas futuras
+  //por hora apenas exibe a página de carrinho
+}
