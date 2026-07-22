7c7f2e71a03a0be9e645eb951ba3f247e8b1ac03
adam castro <riuksilva72@gmail.com>
2026-07-22 16:07:54 -0300

    feat(app): adiciona botões login e logout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router'; //adiciona importação do RoutOutlet e RouteLink;
+import { usuarioLogado, login, logout } from './core/auth';
 //import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
@@ -10,4 +11,7 @@ import { RouterOutlet, RouterLink } from '@angular/router'; //adiciona importaç
 export class App {
   protected readonly title = signal('e-commerce');
   nomeLoja = 'CASAS VAZIAS';
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }
