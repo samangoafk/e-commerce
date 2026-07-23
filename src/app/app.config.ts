3682da27eae52e5e838f5f626fe1b38fc0c755b3
adam castro <riuksilva72@gmail.com>
2026-07-23 13:29:43 -0300

    feat(app.config.ts): adiciona novo import WithFetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,5 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(withFetch()),
   ]
 };
