927b9e59c5f9737c6a8937ed977339a97ae5e5a7
adam castro <riuksilva72@gmail.com>
2026-08-04 17:05:38 -0300

    feat(app.ts): adiciona novo impor header e matanchor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router'; //adiciona importação do RoutOutlet e RouteLink;
+import { RouterOutlet } from '@angular/router'; //adiciona importação do RoutOutlet e RouteLink;
 import { usuarioLogado, login, logout } from './core/auth';
+import { Header } from './shared/layout/header/header';
+import { UpperCasePipe } from '@angular/common';
+import { MatAnchor } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 //import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, RouterLink ],
+  imports: [ RouterOutlet, RouterLink, Header, MatAnchor ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce'); 
 
-  nomeLoja = 'CASAS VAZIA';
+  //!retirei o nome da loja
   usuarioLogado = usuarioLogado;
   login = login;
   logout = logout;
