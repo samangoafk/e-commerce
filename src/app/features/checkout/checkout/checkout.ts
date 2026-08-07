dcd76943f9b7fb52914156c5ee12df0efaf3d689
adam castro <riuksilva72@gmail.com>
2026-08-07 15:09:06 -0300

    feat(checkout.ts): adiciona novos imports para modificar o prenchimento de caixas com minímo de carateres e números

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -3,24 +3,38 @@ import { inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
+import { ValidationErrors } from '@angular/forms';
 import { CarrinhoService } from '../../../core/services/carrinho.service';
 
 @Component({
   selector: 'app-checkout',
-  imports: [ ReactiveFormsModule,  ],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
-export class Checkout {
+export class Checkout { 
+
   carrinhoService = inject(CarrinhoService);
   formulario = new FormGroup({
-    nome: new FormControl(''),
-    email: new FormControl(''),
-    endereco: new FormControl(''),
+    nome: new FormControl('', [Validators.required, Validators.minLength(3), nomeSemNumeros]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    endereco: new FormControl('', [Validators.required, Validators.minLength(5)]),
   });
 
   finalizar (){
     console.log('Dados do Formulário: ', this.formulario.value);
     console.log('Itens do Carrinho: ', this.carrinhoService.itens());
   }
+
+}
+function nomeSemNumeros(control: AbstractControl): ValidationErrors | null {
+  const valor = control.value;
+  if (!valor) return null;
+
+  if (/\d/.test(valor)){
+    return {numeroInvalido: true};
+  }
+  return null;
 }
