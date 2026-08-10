c83c35c5ed87d941caa5c92f45b98ef41c62df37
adam castro <riuksilva72@gmail.com>
2026-08-10 15:57:31 -0300

    feat: Adiciona novo método carrinhoVazio

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -3,13 +3,18 @@ import { signal } from "@angular/core";
 import { computed } from "@angular/core";
 
 
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+}
+
 @Injectable({
     providedIn:'root'
 })
 
 export class CarrinhoService{
 //! Estado global
-private carrinho = signal<{nome: string; preco: number}[]>([]);
+private carrinho = signal<ItemCarrinho[]>([]);
 
 //? Seletores
 itens = computed(()=> this.carrinho());
@@ -17,8 +22,10 @@ quantidadeItens = computed(() => this.carrinho().length); //! Quantidade de Iten
 totalItens = computed (()=> 
      this.carrinho().reduce((total, item)=> total + item.preco, 0)
 );
+carrinhoVazio = computed(()=> this.carrinho().length === 0); //metodo
+
 // TODO: Ações
-adicionar(produto: {nome: string; preco: number}){
+adicionar(produto: ItemCarrinho){
     this.carrinho.update(lista=> [...lista, produto]);
 }
 
