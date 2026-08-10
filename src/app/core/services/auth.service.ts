d1fd8341d5a0407c0740641a7728b406c0976ba8
adam castro <riuksilva72@gmail.com>
2026-08-10 17:12:43 -0300

    feat(auth): adiciona autenticação com signals e token JWT

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@angular/core";
+import { signal } from "@angular/core";
+import { computed } from "@angular/core";
+
+type Usuario = {
+    email: string;
+    perfil: 'usuario';
+}
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthService {
+    private usuario = signal<Usuario | null > (null);
+    private tokenJwt = signal <string | null > (null);
+
+    usuarioAtual = computed(()=> this.usuario());
+    token = computed(()=> this.tokenJwt());
+    usuarioLogado = computed(()=> this.usuario() !== null);
+
+    login (email: string, senha: string): boolean{
+          if (!email || !senha){
+            return false;
+          }
+          const tokenSimulado = 
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
+          'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' +
+          'assinatura-simulada';
+          this.usuario.set({
+              email,
+              perfil: 'usuario',
+          });
+
+          this.tokenJwt.set(tokenSimulado);  
+          return true;
+
+    }
+    logout(){
+        this.usuario.set(null);
+        this.tokenJwt.set(null);
+    }
+    obterToken(): string | null{
+        return this.tokenJwt();
+
+    }
+}
