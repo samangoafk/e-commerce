d9497794bbc55e1c3862b637d59cdb603e1c8327
adam castro <riuksilva72@gmail.com>
2026-08-11 13:54:00 -0300

    feat(auth): adiciona guard para proteção de rotas autenticadas

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,6 +1,14 @@
 import { CanActivateFn } from "@angular/router";
-import { usuarioLogado } from "./auth";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "./services/auth.service";
 
 export const authGuard: CanActivateFn = () => {
-    return usuarioLogado ();
-};
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    if(authService.usuarioLogado()){
+        return true;
+    }
+    return router.createUrlTree(['/login']);
+}
