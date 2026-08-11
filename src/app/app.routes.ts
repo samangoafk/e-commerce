ba4f3cdc1eba1bc673c5f2a1c223f2474574ecda
adam castro <riuksilva72@gmail.com>
2026-08-11 17:04:17 -0300

    feat: Adiciona uma rota de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -62,6 +62,11 @@ export const routes: Routes = [
    loadComponent: () =>
       import ('./features/checkout/checkout/checkout').then((m) =>m.Checkout ),
 },
+{
+   path:'login',
+   loadComponent: ()=>
+      import ('./features/login/login/login').then((m) => m.Login),
+},
 
 {
    path:'**',
