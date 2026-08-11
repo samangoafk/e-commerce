3c50657d7d278aa9165fa151f2eca92634b4d062
adam castro <riuksilva72@gmail.com>
2026-08-11 17:06:39 -0300

    refactor: retira os strings na export class

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router'; //adiciona importação do RoutOutlet e RouteLink;
-import { usuarioLogado, login, logout } from './core/auth';
 import { Header } from './shared/layout/header/header';
-import { UpperCasePipe } from '@angular/common';
 import { MatAnchor } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 //import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
@@ -15,8 +13,5 @@ import { RouterLink } from '@angular/router';
 export class App {
   protected readonly title = signal('e-commerce'); 
 
-  //!retirei o nome da loja
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
+  
 }
