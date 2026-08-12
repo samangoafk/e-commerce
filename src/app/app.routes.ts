d2bc7f720f64f8016c90d0802e26b7526f7d4692
adam castro <riuksilva72@gmail.com>
2026-08-12 17:25:19 -0300

    feat: adicona nova rota de usuário

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,6 +33,7 @@
 
 import { Routes } from "@angular/router";
 import { authGuard } from "./core/auth.guard";
+import { adminGuard } from "./core/admin.guard";
 
 export const routes: Routes = [
 
@@ -66,6 +67,12 @@ export const routes: Routes = [
    path:'login',
    loadComponent: ()=>
       import ('./features/login/login/login').then((m) => m.Login),
+},
+{
+path:'admin',
+canActivate: [adminGuard],
+loadComponent: () =>
+   import('./features/admin/admin/admin').then((m) => m.Admin),
 },
 
 {
