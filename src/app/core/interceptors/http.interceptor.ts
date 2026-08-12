b686623101194411156bda5bf7c12566cea79587
adam castro <riuksilva72@gmail.com>
2026-08-12 17:24:23 -0300

    feat: adiciona erro 403 e redirecionamento de usuário sem permissão para produtos

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -4,10 +4,12 @@ import { catchError } from "rxjs";
 import { throwError } from "rxjs";
 import { inject } from "@angular/core";
 import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
 
 export const httpInterceptor: HttpInterceptorFn =(req, next) => {
 
     const authService = inject(AuthService);
+    const router = inject(Router);
 
     //! NOVO METODO TOKEN
     const token = authService.obterToken();
@@ -29,12 +31,21 @@ export const httpInterceptor: HttpInterceptorFn =(req, next) => {
             next: (event) => console.log('RESPONDE: ', event),
             error: (error) => console.log ('ERRO: ', error)
         }),
+
         catchError((error) => {
             console.error('ERROR GLOBAL: ', error);
 
         if (error.status === 401){
             console.warn('Não Autorizado!');
+            authService.logout();
+            router.navigateByUrl('/login');
         }
+
+        if(error.status === 403){
+            console.warn('Acesso Negado, perfil sem permissão!')
+            router.navigateByUrl('/produtos')
+        } 
+
         if (error.status === 500){
             console.warn('Erro Interno do servidor!');
 
