b18fc89bcda7b4041a09f1daacdfa44f5623c1f1
adam castro <riuksilva72@gmail.com>
2026-08-13 16:35:39 -0300

    feat: adiciona novo import e injeta AuthService para o botão sair

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router'; 
+import {AuthService} from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-acesso-negado',
-  imports: [],
+  imports: [ RouterLink],
   templateUrl: './acesso-negado.html',
   styleUrl: './acesso-negado.css',
 })
-export class AcessoNegado {}
+export class AcessoNegado {
+  private authService = inject(AuthService);
+  sair = this.authService.logout.bind(this.authService);
+
+}
