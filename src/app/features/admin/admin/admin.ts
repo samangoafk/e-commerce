6354c2521f681b891417ff9539792d414c5ea1af
adam castro <riuksilva72@gmail.com>
2026-08-13 16:44:15 -0300

    feat: adiciona autenticação de usuário

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,4 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+export class Admin {
+
+  private authservice = inject(AuthService);
+  private router = inject(Router);
+
+  //! SIMULAÇÂO = Indicadores
+  totalProdutosCadastrados = signal(20);
+  pedidosPendentes = signal(3);
+  usuariosCadastrados = signal(8);
+  
+
+  usuarioAtual = this.authservice.usuarioAtual;
+
+  mensagemPerfil = computed(() => {
+    const usuario =this.usuarioAtual();
+    if (!usuario){
+      return('Nenhum usuário Autenticado!')
+    }
+    return `Usuário autenicado como: $(usuario.perfil)`
+  });
+
+  sair(){
+    this.authservice.logout(),
+    this.router.navigateByUrl('/login')
+  }
+}
