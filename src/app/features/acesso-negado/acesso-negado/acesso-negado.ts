cc542f6b407efffd0a4607f7c696ff9ca1145545
adam castro <riuksilva72@gmail.com>
2026-08-13 14:33:25 -0300

    feat: adiciona pasta acesso-negado

---
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acesso-negado',
+  imports: [],
+  templateUrl: './acesso-negado.html',
+  styleUrl: './acesso-negado.css',
+})
+export class AcessoNegado {}
