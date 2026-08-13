092a59b33808a7d85f5f11339ca8d4cbf97408b5
adam castro <riuksilva72@gmail.com>
2026-08-13 13:40:28 -0300

    feat: Adiciona nova classe de exportação admin

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,33 +2,44 @@ import { Injectable } from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
 
+
+
+
+type PerfilUsuario = 'admin' | 'usuario';
+
 type Usuario = {
     email: string;
-    perfil: 'usuario';
+    perfil: PerfilUsuario;
 }
 @Injectable({
     providedIn: 'root'
 })
 
+
 export class AuthService {
+
+    
     private usuario = signal<Usuario | null > (null);
     private tokenJwt = signal <string | null > (null);
 
     usuarioAtual = computed(()=> this.usuario());
     token = computed(()=> this.tokenJwt());
     usuarioLogado = computed(()=> this.usuario() !== null);
+    admin = computed(()=> this.usuario()?.perfil === 'admin');
 
     login (email: string, senha: string): boolean{
           if (!email || !senha){
             return false;
           }
+          const perfil: PerfilUsuario = email === 'admin@email.com' ? 'admin' : 'usuario';
           const tokenSimulado = 
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
           'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' +
           'assinatura-simulada';
+
           this.usuario.set({
               email,
-              perfil: 'usuario',
+              perfil,
           });
 
           this.tokenJwt.set(tokenSimulado);  
