3a6037d64d5c9356d49d6a8368f97b214adefbfd
adam castro <riuksilva72@gmail.com>
2026-08-13 13:45:34 -0300

    feat: injeta nova rota login

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule}  from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
 import { inject } from '@angular/core';
 import { CarrinhoService } from '../../../core/services/carrinho.service';
@@ -16,13 +16,19 @@ import { AuthService } from '../../../core/services/auth.service';
 })
 export class Header {
   nomeLoja = 'CASAS VAZIA'; //? nome do ecommerce
-  public carrinhoService = inject(CarrinhoService);
+
+  private carrinhoService = inject(CarrinhoService);
   quantidade = this.carrinhoService.quantidadeItens;
   private authService = inject(AuthService);
+
   usuarioLogado = this.authService.usuarioLogado;
   usuarioAtual = this.authService.usuarioAtual;
+
+  private router = inject(Router);
+
   sair(){
     this.authService.logout();
+    this.router.navigateByUrl('/login');
   }
   
 
