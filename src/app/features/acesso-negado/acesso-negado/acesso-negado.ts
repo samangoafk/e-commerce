ae01df4ff72be2838efc8db79dabc77b19a19535
adam castro <riuksilva72@gmail.com>
2026-08-14 15:32:07 -0300

    feat(acesso-negado.ts): Adiciona imports e injeta formatação estilizada para os botões

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -1,15 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router'; 
 import {AuthService} from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-acesso-negado',
-  imports: [ RouterLink],
+  imports: [ RouterLink, MatButtonModule],
   templateUrl: './acesso-negado.html',
   styleUrl: './acesso-negado.css',
 })
 export class AcessoNegado {
   private authService = inject(AuthService);
-  sair = this.authService.logout.bind(this.authService);
+  private router= inject(Router);
+
+  sair() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+    return;
+  }
+  
 
 }
