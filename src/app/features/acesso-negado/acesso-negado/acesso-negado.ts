4d5573328e4f0412159e8ece222aba17169ce051
adam castro <riuksilva72@gmail.com>
2026-08-18 13:50:46 -0300

    refactor(acesso-negado.ts): troca authService por authFacade

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router'; 
-import {AuthService} from '../../../core/services/auth.service';
+import { AuthFacade } from '../../../core/facades/auth.facade';
 import { Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -11,11 +11,11 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './acesso-negado.css',
 })
 export class AcessoNegado {
-  private authService = inject(AuthService);
+  private authFacade = inject(AuthFacade);
   private router= inject(Router);
 
   sair() {
-    this.authService.logout();
+    this.authFacade.sair();
     this.router.navigateByUrl('/login');
     return;
   }
