2dadfa7d546632b09be57cc1e98c6505ff3fc417
adam castro <riuksilva72@gmail.com>
2026-08-19 16:02:40 -0300

    feat(carrinho.services): adiciona novo método que salva os utens no carrinho do usuário mesmo após a saída do mesmo

--- src/app/core/guards/admin.guard.ts
+++ src/app/core/guards/admin.guard.ts
@@ -2,7 +2,7 @@
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { CanActivateFn } from "@angular/router";
-import { AuthFacade } from "./facades/auth.facade";
+import { AuthFacade } from "../facades/auth.facade";
 
 export const adminGuard: CanActivateFn = () => {
     const router = inject(Router);
