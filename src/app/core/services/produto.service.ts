e8f4c5cf12f4fdd8ef1ee90fa80240e427ecdce3
adam castro <riuksilva72@gmail.com>
2026-08-21 13:48:24 -0300

    refactor(produto.service.ts): Adiciona novo import ProdutoLoja

--- src/app/core/services/produto.service.ts
+++ src/app/core/services/produto.service.ts
@@ -1,30 +1,31 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
-import { Inject } from "@angular/core";
+import { ProdutoLoja } from "../models/produto-loja";
 
     type ProdutoApi = {
         title: string;
         price: number;
     };
-    type Produto = {
-        nome: string;
-        preco: number;
-    };
+    // type Produto = {
+    //     nome: string;
+    //     preco: number;
+    // };
+    
 
     @Injectable({providedIn: 'root'})
       export class produtoService {
 
         private http = inject(HttpClient); 
 
-        private API = 'https://fakestoreapi.com/products';
+        private readonly API = 'https://fakestoreapi.com/products';
 
         buscarProdutos(){
             return this.http.get< ProdutoApi []>(this.API);
         }
-        transformarProdutos( dados: ProdutoApi[] ): Produto[] {
-            return dados.map((p) =>({
-                nome: p.title,
-                preco: p.price,
+        transformarProdutos( dados: ProdutoApi[] ): ProdutoLoja[] {
+            return dados.map((produto) =>({
+                nome: produto.title,
+                preco: produto.price,
             }));
         }
     }
